25e5fb351b787362625e51f123f3e614c1129280
Hiroyokii <138383265+Hiroyokii@users.noreply.github.com>
2025-04-19 17:30:28 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        String[] rps = {"Rock", "Paper", "Scissor"};
+
+        System.out.print("Enter your choice (Rock, Paper, Scissor): ");
+        String userChoice = input.nextLine();
+        
+        Random random = new Random();
+        int randomIndex = random.nextInt(3);
+        String computerChoice = rps[randomIndex];
+         
+        System.out.println("Computer chose: " + computerChoice);
+
+        if (userChoice.equalsIgnoreCase(computerChoice)) {
+            System.out.println("It's a Draw!");
+        } else if (userChoice.equalsIgnoreCase("Rock") && computerChoice.equals("Scissor") ||
+                   userChoice.equalsIgnoreCase("Paper") && computerChoice.equals("Rock") ||
+                   userChoice.equalsIgnoreCase("Scissor") && computerChoice.equals("Paper")) {
+
+                System.out.println("You Win!");
+        } else {
+                System.out.println("You Lose!");
+        }
+
+    input.close();
+
+    }
+}
